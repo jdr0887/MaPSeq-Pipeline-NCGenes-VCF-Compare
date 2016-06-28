8ef885885f797fce5d084b79028bf01c3d888e93
Jason Reilly <jdr0887@gmail.com>
2016-06-28 14:50:22 -0400

    using new constructor

--- ncgenes-vcf-compare-commands/src/main/java/edu/unc/mapseq/commands/ncgenes/vcfcompare/RegisterToIRODSAction.java
+++ ncgenes-vcf-compare-commands/src/main/java/edu/unc/mapseq/commands/ncgenes/vcfcompare/RegisterToIRODSAction.java
@@ -1,5 +1,6 @@
 package edu.unc.mapseq.commands.ncgenes.vcfcompare;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.apache.karaf.shell.api.action.Action;
@@ -11,8 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.unc.mapseq.commons.ncgenes.vcfcompare.RegisterToIRODSRunnable;
-import edu.unc.mapseq.config.MaPSeqConfigurationService;
 import edu.unc.mapseq.dao.MaPSeqDAOBeanService;
+import edu.unc.mapseq.dao.MaPSeqDAOException;
+import edu.unc.mapseq.dao.model.WorkflowRun;
+import edu.unc.mapseq.workflow.SystemType;
 
 @Command(scope = "ncgenes-vcfcompare", name = "register-to-irods", description = "Register a sample output to iRODS")
 @Service
@@ -23,29 +26,38 @@ public class RegisterToIRODSAction implements Action {
     @Reference
     private MaPSeqDAOBeanService maPSeqDAOBeanService;
 
-    @Reference
-    private MaPSeqConfigurationService maPSeqConfigurationService;
-
     @Option(name = "--sampleId", description = "Sample Identifier", required = false, multiValued = false)
     private Long sampleId;
 
     @Option(name = "--flowcellId", description = "Flowcell Identifier", required = false, multiValued = false)
     private Long flowcellId;
 
+    @Option(name = "--workflowRunId", description = "WorkflowRun Identifier", required = true, multiValued = false)
+    private Long workflowRunId;
+
+    @Option(name = "--includeMarkDuplicates", description = "includeMarkDuplicates", required = false, multiValued = false)
+    private Boolean includeMarkDuplicates = Boolean.TRUE;
+
     @Override
     public Object execute() {
-        logger.info("ENTERING doExecute()");
+        logger.debug("ENTERING execute()");
+        try {
+            WorkflowRun workflowRun = maPSeqDAOBeanService.getWorkflowRunDAO().findById(workflowRunId);
 
-        RegisterToIRODSRunnable runnable = new RegisterToIRODSRunnable();
-        runnable.setMapseqDAOBeanService(maPSeqDAOBeanService);
-        runnable.setMapseqConfigurationService(maPSeqConfigurationService);
-        if (sampleId != null) {
-            runnable.setSampleId(sampleId);
+            ExecutorService es = Executors.newSingleThreadExecutor();
+            RegisterToIRODSRunnable runnable = new RegisterToIRODSRunnable(maPSeqDAOBeanService, SystemType.EXPERIMENTAL,
+                    workflowRun.getName(), includeMarkDuplicates);
+            if (sampleId != null) {
+                runnable.setSampleId(sampleId);
+            }
+            if (flowcellId != null) {
+                runnable.setFlowcellId(flowcellId);
+            }
+            es.submit(runnable);
+            es.shutdown();
+        } catch (MaPSeqDAOException e) {
+            e.printStackTrace();
         }
-        if (flowcellId != null) {
-            runnable.setFlowcellId(flowcellId);
-        }
-        Executors.newSingleThreadExecutor().execute(runnable);
         return null;
     }
 
